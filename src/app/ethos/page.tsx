773dac76b4c922aaaa1695a6db82e89567d1d83d
Zafeer <contact@zafeer.pk>
2025-08-20 16:02:39 +0500

    fix: ethos issues

--- src/app/ethos/page.tsx
+++ src/app/ethos/page.tsx
@@ -17,7 +17,7 @@ import Image5 from "@/assets/ethos/image5.jpg";
 export default function Home() {
   return (
     <div className='relative min-h-screen overflow-hidden'>
-      {/* Background Video */}
+      {/* Background Image */}
       <div className='absolute inset-0 z-0'>
         <Image
           src={BGImage}
@@ -30,62 +30,64 @@ export default function Home() {
       <Header logo={Logo} isShowMenu={false} />
 
       {/* Main Content */}
-      <main className='relative z-10 container  -mt-4'>
-        <HorizontalScrollContainer className=''>
+      <main className='relative z-10 container -mt-4'>
+        <HorizontalScrollContainer>
           {/* text content */}
-          <div className='max-w-[900px] min-w-[900px] pl-30'>
-            <h1 className='text-[#e7e7dc]  text-4xl'>THE ETHOS</h1>
-            <h2 className='text-[#e7e7dc] font-[300] text-2xl'>Our work is guided by feeling.</h2>
+          <div className='max-w-[900px] min-w-[900px] pl-30 md:min-w-[80vw] md:max-w-[80vw] sm:min-w-[70vw] sm:max-w-[70vw]'>
+            <h1 className='text-[#e7e7dc] text-4xl md:text-3xl sm:text-2xl'>THE ETHOS</h1>
+            <h2 className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>Our work is guided by feeling.</h2>
 
-            <h3 className='text-[#e7e7dc]  text-xl font-sans leading-4 mt-14'>Peace</h3>
-            <p className='text-[#e7e7dc] font-sans font-light text-[22px]'>
-              The design should ground you. Light, air, and materials that calm the nervous system.{" "}
+            <h3 className='text-[#e7e7dc] text-xl md:text-lg font-sans leading-4 mt-14'>Peace</h3>
+            <p className='text-[#e7e7dc] font-sans font-light text-[22px] md:text-[18px] sm:text-[16px]'>
+              The design should ground you. Light, air, and materials that calm the nervous system.
             </p>
 
             {/* verticle bar */}
             <div className='ml-2 mt-2 mb-6 w-px h-8 bg-white/40'></div>
 
-            <h3 className='text-[#e7e7dc]  text-[22px] font-sans leading-4'>Simplicity</h3>
-            <p className='text-[#e7e7dc]  font-sans font-light text-[22px]'>
-              Nothing extra. Everything necessary. Beauty found in restraint.{" "}
+            <h3 className='text-[#e7e7dc] text-[22px] md:text-[20px] font-sans leading-4'>Simplicity</h3>
+            <p className='text-[#e7e7dc] font-sans font-light text-[22px] md:text-[18px] sm:text-[16px]'>
+              Nothing extra. Everything necessary. Beauty found in restraint.
             </p>
 
             {/* verticle bar */}
             <div className='ml-2 mt-2 mb-6 w-px h-8 bg-white/40'></div>
 
-            <h3 className='text-[#e7e7dc]  text-[22px] font-sans leading-4'>Flow</h3>
-            <p className='text-[#e7e7dc]  font-sans font-light text-[22px]'>
+            <h3 className='text-[#e7e7dc] text-[22px] md:text-[20px] font-sans leading-4'>Flow</h3>
+            <p className='text-[#e7e7dc] font-sans font-light text-[22px] md:text-[18px] sm:text-[16px]'>
               From one room to the next, one breath to the next—our work moves like water.
             </p>
           </div>
 
           {/* image 1 */}
-          <div className='relative max-w-[50vw] min-w-[50vw] h-[60vh] ml-[30vw]'>
-            <div className='absolute bg-black/70 text-xl font-light text-white/80 w-[550px] h-[300px] px-10 py-5 bottom-0 right-0 flex justify-center items-center text-right'>
-              The stone sink rests on a reclaimed timber <br /> surface, its raw form echoing in imperfection. <br /> A
-              steel-framed mirror adds structure, while the <br /> lighting — placed at face level — was carefully{" "}
-              <br /> considered to avoid harsh overhead shadows.
+          <div className='relative max-w-[50vw] min-w-[850px] h-[60vh] ml-[10vw]'>
+            <div className='absolute bg-black/70 text-xl font-light text-white/80 w-[55%] h-[50%] px-6 py-5 bottom-0 right-0 flex justify-center items-center text-right leading-relaxed'>
+              <span className='block'>
+                The stone sink rests on a reclaimed timber surface, its raw form echoing in imperfection. A steel-framed
+                mirror adds structure, while the lighting — placed at face level — was carefully considered to avoid
+                harsh overhead shadows.
+              </span>
             </div>
             <Image src={Image1} alt='' className='w-full h-full object-cover' />
           </div>
 
           {/* image 2 */}
-          <div className='relative max-w-[50vw] min-w-[50vw] ml-[5vw] h-[60vh]'>
+          <div className='relative w-full h-[60vh] ml-[5vw] min-w-[850px]'>
             <Image src={Image2} alt='' className='w-full h-full object-cover' />
           </div>
 
           {/* image 3 */}
-          <div className='relative max-w-[50vw] min-w-[50vw] ml-[5vw] h-[60vh]'>
+          <div className='relative w-full h-[60vh] ml-[5vw] min-w-[850px]'>
             <Image src={Image3} alt='' className='w-full h-full object-cover' />
           </div>
 
           {/* image 4 */}
-          <div className='relative max-w-[50vw] min-w-[50vw] ml-[5vw] h-[60vh]'>
+          <div className='relative w-full h-[60vh] ml-[5vw] min-w-[850px]'>
             <Image src={Image4} alt='' className='w-full h-full object-cover' />
           </div>
 
           {/* image 5 */}
-          <div className='relative max-w-[50vw] min-w-[50vw] ml-[5vw] h-[60vh]'>
+          <div className='relative w-full h-[60vh] ml-[5vw] min-w-[850px]'>
             <Image src={Image5} alt='' className='w-full h-full object-cover' />
           </div>
           <div className='min-w-[30vw]'></div>
