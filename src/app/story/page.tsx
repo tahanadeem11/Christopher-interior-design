51df5d9e1ca1bf0356b9cb8d0fd797dc33499f6b
Zafeer <contact@zafeer.pk>
2025-08-21 03:27:06 +0500

    feat: add navigation links and improve story page layout with proper spacing

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import Image from "next/image";
 import { useState, useRef, useEffect } from "react";
 import Header from "@/components/Header";
@@ -80,8 +81,10 @@ export default function Story() {
             <br />
             <br />
 
-            <h2 className='text-[#e7e7dc] font-[300] text-lg md:text-lg sm:text-lg uppercase'>FUTURE PROJECTS</h2>
-            <div className='-mt-2 w-8 h-px bg-white/50'></div>
+            <Link href='/projects'>
+              <h2 className='text-[#e7e7dc] font-[300] text-lg md:text-lg sm:text-lg uppercase'>FUTURE PROJECTS</h2>
+              <div className='-mt-2 w-8 h-px bg-white/50'></div>
+            </Link>
           </div>
 
           {/* image 1 */}
@@ -93,21 +96,29 @@ export default function Story() {
           />
 
           <div className='relative lg:w-auto w-full h-auto lg:ml-0 flex flex-col items-center px-6 my-10 lg:my-0 lg:px-0 lg:min-w-[100vw]'>
-            <div className='lg:w-full w-full'>
-              <ImageZoom
-                src={Image2}
-                alt=''
-                className='w-full lg:h-full h-auto object-cover'
-                containerClassName='relative w-full lg:h-[55vh]'
-              />
-              <div className='flex lg:flex-row flex-col justify-start gap-4 lg:gap-10 mt-4'>
-                <div className='flex flex-col'>
-                  <h2 className='text-[#e7e7dc] font-[300] text-lg md:text-lg sm:text-lg uppercase'>THE ETHOS</h2>
-                  <div className='-mt-2 w-8 h-px bg-white/50'></div>
-                </div>
-                <div className='flex flex-col'>
-                  <h2 className='text-[#e7e7dc] font-[300] text-lg md:text-lg sm:text-lg uppercase'>FUTURE PROJECTS</h2>
-                  <div className='-mt-2 w-8 h-px bg-white/50'></div>
+            <div className='lg:w-full w-full flex justify-center'>
+              <div>
+                <ImageZoom
+                  src={Image2}
+                  alt=''
+                  className='w-full lg:h-full h-auto object-cover'
+                  containerClassName='relative w-[50vw] lg:h-[55vh]'
+                />
+                <div className='flex lg:flex-row flex-col justify-start gap-4 lg:gap-10 mt-4'>
+                  <div className='flex flex-col'>
+                    <Link href='/ethos'>
+                      <h2 className='text-[#e7e7dc] font-[300] text-lg md:text-lg sm:text-lg uppercase'>THE ETHOS</h2>
+                      <div className='-mt-2 w-8 h-px bg-white/50'></div>
+                    </Link>
+                  </div>
+                  <div className='flex flex-col'>
+                    <Link href='/projects'>
+                      <h2 className='text-[#e7e7dc] font-[300] text-lg md:text-lg sm:text-lg uppercase'>
+                        FUTURE PROJECTS
+                      </h2>
+                      <div className='-mt-2 w-8 h-px bg-white/50'></div>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
@@ -121,6 +132,7 @@ export default function Story() {
               life. For over two decades, I worked internationally as a model. I spent years inside beautifully styled
               spaces — apartments, homes, studios — crafted for the camera.
             </p>
+            <br />
             <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
               At first, it all looked impressive. But the longer I was inside these environments, the more I began to
               feel something was missing. They were visually perfect. But emotionally flat. They weren’t designed to be
@@ -131,11 +143,6 @@ export default function Story() {
               That realisation sat with me for a long time. I didn’t know what to do with it — only that I was paying
               attention to things most people overlooked: the way light moved across old brick, the softness of aged
               wood, how stillness made a room feel more generous.
-            </p>
-            <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
-              The shift came when my partner left a high-pressure job. We both needed a reset — not just in lifestyle,
-              but in environment. We began restoring properties, working directly on homes from the inside out. It
-              wasn’t a business plan. It was instinct.
             </p>
           </div>
 
@@ -150,25 +157,46 @@ export default function Story() {
           {/* section 3 */}
           <div className='lg:max-w-[900px] lg:min-w-[900px] lg:pl-30 w-full max-w-[900px] px-6'>
             <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
+              The shift came when my partner left a high-pressure job. We both needed a reset — not just in lifestyle,
+              but in environment. We began restoring properties, working directly on homes from the inside out. It
+              wasn’t a business plan. It was instinct.
+            </p>
+            <br />
+            <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
               I was on-site every day — sketching ideas, adapting floorplans, learning from the trades, figuring out
               what it meant to make a space feel right instead of just look right. And then something happened I didn’t
               expect: People who visited our spaces would say, &quot;I feel different in here.&quot; &quot;It’s
               calm.&quot; &quot;I don’t want to leave.&quot;
             </p>
+            <br />
             <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
               That’s when I knew I had found the work I was meant to do. I wasn’t interested in building fast or
               flipping for profit. I was interested in creating homes that offered something deeper — connection,
               clarity, a sense of belonging.
             </p>
+          </div>
+
+          {/* image 4 */}
+          <ImageZoom
+            src={Image4}
+            alt=''
+            className='w-full lg:h-full h-auto object-cover'
+            containerClassName='relative lg:max-w-[50vw] lg:min-w-[400px] lg:h-[60vh] lg:ml-[10vw] w-full h-auto px-6 my-10 lg:my-0'
+          />
+
+          {/* section 5 */}
+          <div className='lg:max-w-[900px] lg:min-w-[900px] lg:pl-30 w-full max-w-[900px] px-6'>
             <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
               So I kept going. I studied what I felt drawn to — the lived-in elegance of Japanese design, the
               imperfections that make materials human, the way natural light shapes emotion. I spent time in places
               where resourcefulness is a necessity, not an aesthetic — and brought those lessons home.
             </p>
+            <br />
             <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
               Today, every project I take on begins the same way: with listening. Not just to the client, but to the
               building. The surroundings. The light.
             </p>
+            <br />
             <p className='text-[#e7e7dc] font-[300] text-2xl md:text-xl sm:text-lg'>
               Because a house isn’t just a structure. It’s a container for how people live, rest, gather, and grow. And
               when it’s done right, it holds you — not just physically, but emotionally. That’s why I do this. Not to
@@ -176,9 +204,9 @@ export default function Story() {
             </p>
           </div>
 
-          {/* image 4 */}
+          {/* image 5 */}
           <ImageZoom
-            src={Image4}
+            src={Image1}
             alt=''
             className='w-full lg:h-full h-auto object-cover'
             containerClassName='relative lg:max-w-[50vw] lg:min-w-[400px] lg:h-[60vh] lg:ml-[10vw] w-full h-auto px-6 my-10 lg:my-0'
